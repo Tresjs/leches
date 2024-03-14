609956998815081dbf78304fdf19ec668685f760
Shahjahan Sarfraz <110630702+Jahan-Shah@users.noreply.github.com>
2024-03-15 00:25:52 +0500

    docs: fix favicon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     ['link', { 
       rel: 'icon',
       href: `data:image/svg+xml,
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🍰/text></svg>`, 
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y="1em" font-size="90">🍰</text></svg>`, 
     }],
   ],
   themeConfig: {
